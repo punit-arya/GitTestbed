bd2ec2fa05cd1e195bd454d2eb503bb423e786cc
Punit Arya <arya.punit@gmail.com>
2026-04-29 09:46:45 +0530

    Add 1 and 2 to 1.c

--- 1.c
+++ 1.c
@@ -3,4 +3,5 @@ This is master branch.  Making another change in master.
 This is testing2 branch.
 This is testing3 branch.  Added more changes in testing3.
 This is testing4 branch.  Adding a change in here without creating a merge conflict because this edit is in a different location than the edits in the destination branch (master).  Forgot to demo 'git mergetool'.
-  
+  1
+2
